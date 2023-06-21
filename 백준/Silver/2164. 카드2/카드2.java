999b6b36545b04d25df4192c803b1011aa10a3de
이정연 <99dlwjddus@naver.com>
2023-06-21 18:10:09 +0900

    [Silver IV] Title: 카드2, Time: 192 ms, Memory: 45652 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //int형 queue선언, linkedlist이용
+        Queue<Integer> queue = new LinkedList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++){
+            queue.add(i);
+        }
+        while(queue.size() != 1){
+            queue.poll();
+            queue.add(queue.poll());
+        }
+        System.out.println(queue.poll());
+    }
+}
