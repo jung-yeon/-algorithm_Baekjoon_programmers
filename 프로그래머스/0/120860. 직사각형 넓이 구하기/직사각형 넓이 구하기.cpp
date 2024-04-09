ab68579fed74edf72c9318eb5f82c70fc1601526
이정연 <99dlwjddus@naver.com>
2024-04-09 11:44:20 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.cpp
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.cpp
@@ -1,31 +1,34 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
 int solution(vector<vector<int>> dots) {
-    int answer = 0;
-    int xMin = 256;
-    int xMax = -256;
-    int yMin = 256;
-    int yMax = -256;
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 2; j++) {
-            if (dots[i][0] < xMin) {
-                xMin = dots[i][0];
-            }
-            else if (dots[i][1] < yMin) {
-                yMin = dots[i][1];
-            }
-            else if (dots[i][0] > xMax) {
-                xMax = dots[i][0];
-            }
-            else if (dots[i][1] > yMax) {
-                yMax = dots[i][1];
-            }
-        }
+//     int answer = 0;
+//     int xMin = 256;
+//     int xMax = -256;
+//     int yMin = 256;
+//     int yMax = -256;
+//     for (int i = 0; i < 4; i++) {
+//         for (int j = 0; j < 2; j++) {
+//             if (dots[i][0] < xMin) {
+//                 xMin = dots[i][0];
+//             }
+//             else if (dots[i][1] < yMin) {
+//                 yMin = dots[i][1];
+//             }
+//             else if (dots[i][0] > xMax) {
+//                 xMax = dots[i][0];
+//             }
+//             else if (dots[i][1] > yMax) {
+//                 yMax = dots[i][1];
+//             }
+//         }
 
-    }
-    answer = (yMax - yMin) * (xMax - xMin);
+//     }
+//     answer = (yMax - yMin) * (xMax - xMin);
+//     return answer;
+    sort(dots.begin(), dots.end());
+    int answer = (dots[1][1] - dots[0][1]) * (dots[3][0] - dots[0][0]);
     return answer;
 }
